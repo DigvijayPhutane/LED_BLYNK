cecd9d41322429020fc5e44bf8154de9dd027a49
Digvijay Phutane <digvijay.phutane@gmail.com>
2022-07-27 08:43:18 +0530

    Add files via upload

---
+++ ledblynk.ino
@@ -0,0 +1,26 @@
+#define BLYNK_TEMPLATE_ID "TMPLDGYbuY0i"
+#define BLYNK_DEVICE_NAME "led"
+#define BLYNK_AUTH_TOKEN "QMu1Ejo9B0J0PxS_e2zD2ziJYN4tpb1b"
+
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>  
+#include <BlynkSimpleEsp8266.h>
+ 
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "God Of Thunder 2G";  // Enter your Wifi Username
+char pass[] = "Thor@hatoda";  // Enter your Wifi password
+
+int ledpin = D4;
+void setup()
+{     
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);    
+  pinMode(ledpin,OUTPUT);
+}
+
+void loop()
+{
+  Blynk.run(); 
+}
